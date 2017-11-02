79d0879284403f92a2c642f38581abeabd648f25
Elijah Prince <elijahprince73@gmail.com>
2017-11-02 02:23:29 -0700

    Added google maps

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,20 +1,18 @@
-import React,{Component } from 'react';
+import React, { Component } from "react";
 
 class GoogleMap extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
         lng: this.props.lon
       }
-    })
+    });
   }
 
-  render(){
-    //this.refs.map
-    return <div ref='map'/>
+  render() {
+    return <div ref="map" />;
   }
 }
 
